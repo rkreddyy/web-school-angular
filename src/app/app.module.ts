3d5f1a1afcef1188f37f46bfc481fdd12512f4f5
RaviKiran <ravikiran.anumula@gmail.com.com>
2020-10-13 12:29:16 +0000

    added login and logout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseItemComponent } from './course-item/course-item.component';
+import { CreateCourseComponent } from './create-course/create-course.component';
+import { EditCourseComponent } from './edit-course/edit-course.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    CourseListComponent,
+    CourseItemComponent,
+    CreateCourseComponent,
+    EditCourseComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
